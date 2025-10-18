3abb6cd25a5c8879333d9b3912775d30be755f83
DaiLoiProject <dangdailoi732006@gmail.com>
2025-10-18 08:08:54 +0700

    Cập nhật code lên nhánh Demo

---
+++ Scripts/Collections.js/Collections.js
@@ -0,0 +1,17 @@
+﻿const sidebarBtns = document.querySelectorAll('.sidebar-btn');
+
+sidebarBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const submenu = btn.nextElementSibling; // ul.submenu
+        const icon = btn.querySelector('i');   // icon mũi tên
+
+        // Toggle hiển thị submenu
+        if (submenu.style.display === 'block') {
+            submenu.style.display = 'none';
+            icon.style.transform = 'rotate(0deg)'; // icon quay về ban đầu
+        } else {
+            submenu.style.display = 'block';
+            icon.style.transform = 'rotate(180deg)'; // icon xoay xuống
+        }
+    });
+});

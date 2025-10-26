cecb135a35b40f07f5b0e7a3ce254c4142a2e63b
Long 6 mui <thuytinhhoanglong1510@gmail.com>
2025-10-26 15:23:40 +0700

    update user login-logout-register

--- Controllers/TrangChuController.cs
+++ Controllers/TrangChuController.cs
@@ -15,6 +15,8 @@ namespace Webbanhang_nhom10.Controllers
         }
         public ActionResult Shop()
         {
+            if (Session["User"] == null)
+                return RedirectToAction("Login");
             return View();
         }
         public ActionResult Bestseller()
@@ -24,6 +26,47 @@ namespace Webbanhang_nhom10.Controllers
         public ActionResult Collections()
         {
             return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            // Login tạm thời (sau có DB thì thay)
+            if (username == "admin" && password == "123")
+            {
+                Session["User"] = username; //      Lưu user đang đăng nhập
+                ViewBag.Message = "Đăng nhập thành công!";
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Error = "Sai tài khoản hoặc mật khẩu!";
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(string username, string email, string password)
+        {
+            // Chỗ này bạn có thể lưu DB sau, còn giờ chỉ chuyển hướng tạm
+            TempData["Message"] = "Đăng ký thành công, hãy đăng nhập!";
+            return RedirectToAction("Login");
+        }
+
+        // LOGOUT
+        public ActionResult Logout()
+        {
+            Session["User"] = null;
+            return RedirectToAction("Login");
         }
     }
 }
